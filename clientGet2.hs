08d7aa20ffeb52aaa4aa7ce09a769ab5ba7eef9b
Oisin Tiernan <tiernano@tcd.ie>
2016-11-01 15:46:21 +0000

    working client

---
+++ clientGet2.hs
@@ -0,0 +1,23 @@
+import Network
+import System.IO
+import System.Environment (getArgs)
+
+main = withSocketsDo $ do
+    echo
+
+echo :: IO ()
+echo = do
+    putStrLn "what word do you want in caps?"
+    input' <- getLine
+    --h <- connectTo "localhost" (PortNumber 8000)
+    h <- connectTo "www.scss.tcd.ie" (PortNumber 80)
+    hSetBuffering h LineBuffering--means that each character wont consume a whole packet to itself
+    --PutStr h ("GET /echo.php?message=" ++ input' ++ "\nHost: localhost:8000  \r\n\r\n")
+    hPutStr h ("GET /~ebarrett/lectures/cs4032/echo.php?message=" ++ input' ++ "\nHost: www.scss.tcd.ie  \r\n\r\n")
+    --hPutStr h "GET /~ebarrett/lectures/cs4032/echo.php?message=" ++ input' ++ "HTTP/1.1\nHost: www.scss.tcd.ie  \r\n\r\n"
+    contents <- hGetContents h
+    putStrLn (contents)
+    hClose h
+    if (input' == "q")
+        then return()
+        else echo
