94b09d13dde7dc89736e068c1038c2aa4d657454
Oisin Tiernan <tiernano@tcd.ie>
2016-11-03 14:34:19 +0000

    made change

--- clientGet2.hs
+++ clientGet2.hs
@@ -7,13 +7,13 @@ main = withSocketsDo $ do
 
 echo :: IO ()
 echo = do
-    putStrLn "what word do you want in caps?"
+    putStrLn "what word do you want in caps? Enter q to end program"
     input' <- getLine
-    --h <- connectTo "localhost" (PortNumber 8000)
-    h <- connectTo "www.scss.tcd.ie" (PortNumber 80)
+    h <- connectTo "localhost" (PortNumber 8000)
+    --h <- connectTo "www.scss.tcd.ie" (PortNumber 80)
     hSetBuffering h LineBuffering--means that each character wont consume a whole packet to itself
-    --PutStr h ("GET /echo.php?message=" ++ input' ++ "\nHost: localhost:8000  \r\n\r\n")
-    hPutStr h ("GET /~ebarrett/lectures/cs4032/echo.php?message=" ++ input' ++ "\nHost: www.scss.tcd.ie  \r\n\r\n")
+    hPutStr h ("GET /echo.php?message=" ++ input' ++ "\nHost: localhost:8000  \r\n\r\n")
+    --hPutStr h ("GET /~ebarrett/lectures/cs4032/echo.php?message=" ++ input' ++ "\nHost: www.scss.tcd.ie  \r\n\r\n")
     --hPutStr h "GET /~ebarrett/lectures/cs4032/echo.php?message=" ++ input' ++ "HTTP/1.1\nHost: www.scss.tcd.ie  \r\n\r\n"
     contents <- hGetContents h
     putStrLn (contents)
